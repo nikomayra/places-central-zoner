587aa9e9a3c18fd25e25e7b072349c0bef032419
niknak11 <niknak1188@gmail.com>
2024-07-20 11:47:11 -0400

    Changes:  * location input limits  * Delete buttons

--- frontend/src/components/LocationInput.tsx
+++ frontend/src/components/LocationInput.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { TextField, Button, Box } from '@mui/material';
+import { TextField, Button, Box, IconButton } from '@mui/material';
+import { Delete } from '@mui/icons-material';
 
 const LocationInput: React.FC = () => {
-  const [locations, setLocations] = useState<string[]>(['']);
-  const maxInputs = 5;
+  const [locations, setLocations] = useState<string[]>(['', '']);
+  const maxLocations = 5;
+  const minLocations = 2;
 
   const handleChange = (index: number, value: string) => {
     const newLocations = [...locations];
@@ -12,7 +14,7 @@ const LocationInput: React.FC = () => {
   };
 
   const handleAddLocationInput = () => {
-    if (locations.length >= maxInputs) {
+    if (locations.length >= maxLocations) {
       window.alert('Reached max inputs...');
     } else {
       const newLocations = [...locations];
@@ -22,20 +24,41 @@ const LocationInput: React.FC = () => {
   };
 
   const handleSearch = () => {
-    // Logic to handle search
-    console.log(locations);
+    if (locations.length < minLocations) {
+      window.alert('Minimum 2 locations...');
+    } else {
+      // Logic to handle search
+      console.log(locations);
+    }
+  };
+
+  const handleDeleteLocationInput = (index: number) => {
+    setLocations((prevLocations) =>
+      prevLocations.filter((_, i) => i !== index)
+    );
   };
 
   return (
     <Box display='flex' flexDirection='column' gap={2}>
       {locations.map((location, index) => (
-        <TextField
-          key={index}
-          label={`Location ${index + 1}`}
-          value={location}
-          onChange={(e) => handleChange(index, e.target.value)}
-          variant='outlined'
-        />
+        <Box display='flex' flexDirection='row' gap={1} key={index}>
+          <TextField
+            label={`Location ${index + 1}`}
+            value={location}
+            onChange={(e) => handleChange(index, e.target.value)}
+            variant='outlined'
+            style={{ flexGrow: 1 }}
+          />
+          <IconButton
+            aria-label='delete'
+            size='small'
+            color='error'
+            style={{ flexGrow: 0 }}
+            onClick={() => handleDeleteLocationInput(index)}
+          >
+            <Delete fontSize='small' />
+          </IconButton>
+        </Box>
       ))}
       <Button
         variant='outlined'
