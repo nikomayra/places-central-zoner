20cf19e0ed936fe05c4ca0115adf2bef9e8d0332
niknak11 <niknak1188@gmail.com>
2024-08-28 14:24:12 -0400

    uibuild

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,26 +1,6 @@
 import math
 import numpy as np
 
-# Algorithm to compare 2 strings returning number of operations to convert a to b
-def levenshtein(a, b):
-    matrix = [[0] * len(b) for _ in range(len(a))]
-
-    for i in range(len(a)):
-        matrix[i][0] = i
-
-    for j in range(len(b)):
-        matrix[0][j] = j
-
-    for j in range(len(b)):
-        for i in range(len(a)):
-            matrix[i][j] = min(
-                (matrix[i - 1][j] if i > 0 else 0) + 1,
-                (matrix[i][j - 1] if j > 0 else 0) + 1,
-                (matrix[i - 1][j - 1] if i > 0 and j > 0 else 0) + (0 if a[i] == b[j] else 1)
-            )
-
-    return matrix[len(a) - 1][len(b) - 1]
-
 # Calculates NE and SW points of square around circle
 # radius in meters
 def calculate_bounding_box(center, radius):
