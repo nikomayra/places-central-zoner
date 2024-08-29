4374e10a6af973dfc5c6eab70860e01e9ad074ae
niknak11 <niknak1188@gmail.com>
2024-08-29 13:38:26 -0400

    .

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,11 +5,11 @@ import os
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    # SECRET_KEY = os.getenv('SECRET_KEY')
     GOOGLE_PLACES_API_KEY = os.getenv('GOOGLE_PLACES_API_KEY')
     GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
-    GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
-    REDIRECT_URI = os.getenv('REDIRECT_URI')
+    # GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
+    # REDIRECT_URI = os.getenv('REDIRECT_URI')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REDIS_URL = os.getenv('REDIS_URL')
